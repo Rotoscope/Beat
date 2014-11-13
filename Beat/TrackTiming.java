58f3a9c64e8ae38fe51ce645eb81fe1994a7dafc
Rotoscope <ticktick58@gmail.com>
2014-11-13 00:20:38 -0800

    Create TrackTiming.java

---
+++ Beat/TrackTiming.java
@@ -0,0 +1,16 @@
+public class TrackTiming {
+  int trackNumber, trackSize;
+  
+  TrackTiming() {
+    trackNumber = -1;
+    trackSize = -1;
+  }
+  
+  public void setTrackNumber(int i) {
+    trackNumber = i;
+  }
+  
+  public void setTrackSize(int i) {
+    trackSize = i;
+  }
+}
