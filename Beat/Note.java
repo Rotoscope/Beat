5bbf302509b9490bdead5059a9784f782e906347
Rotoscope <ticktick58@gmail.com>
2014-11-13 00:45:33 -0800

    Update Note.java

--- Beat/Note.java
+++ Beat/Note.java
@@ -1,4 +1,43 @@
 public class Note {
-  Note() {
+  long tick;
+  int channel, velocity, noteKey, octave;
+  String noteName, command;
+  
+  Note(long tic, String com, String nN, int oct, int nK, int vel, int ch) {
+    tick = tic;
+    command = com;
+    noteName = nN;
+    octave = oct;
+    noteKey = nK;
+    velocity = vel;
+    channel = ch;
   }
+  
+  public long getTick() {
+    return tick;
+  }
+  
+  public String getCommand() {
+    return command;
+  }
+  
+  public String getNoteName() {
+    return noteName;
+  }
+  
+  public int getOctave() {
+    return octave;
+  }
+  
+  public int getNoteKey() {
+    return noteKey;
+  }
+   
+  public int getVelocity() {
+    return velocity;
+  }
+  
+  public int getChannel() {
+    return channel;
+  } 
 }
